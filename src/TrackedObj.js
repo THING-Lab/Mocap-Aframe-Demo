1deeff19d9e39893d91a9418f477f497399cf07c
Peter <ptgyory@gmail.com>
2019-02-26 16:19:13 -0700

    Attach mocap data to aframe component

--- src/TrackedObj.js
+++ src/TrackedObj.js
@@ -1,27 +1,36 @@
 // Holds our socket server connection
 let socket;
-const trackedObjListeners = [console.log];
+const trackedObjListeners = [];
 
 function initSockets() {
-  socket = io('192.168.1.125:8000');
-  console.log(socket);
+  socket = io('192.168.1.100:8000');
 
   // Loop through all the obj listeners send them frame data
   socket.on('frame', d => trackedObjListeners.forEach(l => l(d)));
 }
 
 // Default shape is a cube
-AFRAME.registerGeometry('tracked-obj', {
+AFRAME.registerComponent('tracked-obj', {
+  multiple: true,
   schema: {
-    objId: {
+    objID: {
       default: 0,
     },
   },
 
-  init: (data) => {
+  init: function() {
     trackedObjListeners.push(
       (frameData) => {
-        console.log(frameData[data.objId]);
+        const data = frameData[this.data.objID];
+        // How do I calibrate these values. I should probs make them all scaled based on one value passed in
+        if (data) {
+          if (data.x && data.y && data.z) {
+            const mesh = this.el.getObject3D('mesh');
+            mesh.position.x = data.x / 700;
+            mesh.position.y = data.z / 700;
+            mesh.position.z = data.y / 700;
+          }
+        }
         // Use frame data to set position and rotation
       }
     );
